55bf9a01328341389f66fe649d5564153e9f5442
wutpppppppppppppppppp <gansahy@gmail.com>
2025-01-13 23:02:55 +0700

    feat: card style

--- src/components/CatalogCard.tsx
+++ src/components/CatalogCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+import Image from 'next/image';
 import React from 'react';
 
 type CatalogCardProps = {
@@ -6,13 +6,14 @@ type CatalogCardProps = {
   title: string;
 };
 
-export const CatalogCard: React.FC<CatalogCardProps> = ({ alt, title }) => {
+export const CatalogCard: React.FC<CatalogCardProps> = async ({ alt, title }) => {
   return (
-    <div className="flex flex-col border-2 border-black">
-      {alt + title}
+    <div className="flex w-96 flex-col border-2">
+      <Image src="/images/catalog/1.jpg" alt={alt} width={300} height={300} />
+      <h2>{title}</h2>
       <button
         type="button"
-        className="hover:bg-blue-500"
+        className="bg-gradient-to-t from-teal-200 to-violet-500 text-white hover:bg-white hover:text-black"
       >
         เพิ่มเข้าตะกร้า
       </button>
