9e3ed74d3441cc86337dd09b0e9e6e2e0a47292f
Mark169Z <90457798+Mark169Z@users.noreply.github.com>
2025-01-19 17:00:52 +0700

    feat(landing): landing

--- src/app/[locale]/(marketing)/page.tsx
+++ src/app/[locale]/(marketing)/page.tsx
@@ -1,6 +1,6 @@
-import { getTranslations, setRequestLocale } from 'next-intl/server';
-import { Events, Currency, Shuttle } from '@carbon/icons-react';
 import tcm_warehouse from '@/public/assets/images/tcm-warehouse.jpg';
+import { Currency, Email, Events, PhoneFilled, Shuttle, Store } from '@carbon/icons-react';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
 import Image from 'next/image';
 
 type IIndexProps = {
@@ -27,17 +27,54 @@ export default async function Index(props: IIndexProps) {
     locale,
     namespace: 'Index',
   });
+  const articleItems = [
+    'one',
+    'two',
+    'three',
+    'four',
+  ];
+
+  type ArticleKeys = 'one' | 'two' | 'three' | 'four';
 
   return (
     <>
-      <section className="flex h-[calc(0.4*100vw)] w-screen flex-col bg-blue-500">
-        <div className="h-[85%] w-full"></div>
-        <div className="flex h-[15%] w-full bg-[#F4F4F4]">
-
+      <section>
+        <div className="flex h-[calc(0.4*100vw)] w-screen flex-col bg-blue-500"></div>
+        <div className="grid min-h-20 w-full grid-cols-4 divide-x divide-[#CCCCCC] bg-[#F4F4F4] p-4">
+          <div className="flex w-full flex-row items-center justify-center gap-8 px-8">
+            <div className="flex flex-col items-center justify-center gap-2">
+              <PhoneFilled size="32" />
+              <h2 className="m-0">{t(`Tel_title`)}</h2>
+            </div>
+            <div className="flex flex-col">
+              <p className="m-0">{t(`Tel_one`)}</p>
+              <p className="m-0">{t(`Tel_two`)}</p>
+            </div>
+          </div>
+          <div className="flex w-full flex-row items-center justify-center gap-8 px-8">
+            <div className="flex flex-col items-center justify-center gap-2">
+              <Email size="32" />
+              <h2 className="m-0">{t(`Email_title`)}</h2>
+            </div>
+            <p>{t(`Email`)}</p>
+          </div>
+          <div className="flex w-full flex-row items-center justify-center gap-8 px-8">
+            <div className="flex flex-col items-center justify-center gap-2">
+              <Store size="32" />
+              <h2 className="m-0">{t(`Address_title`)}</h2>
+            </div>
+            <div className="flex flex-col">
+              <p className="m-0">{t(`Address_one`)}</p>
+              <p className="m-0">{t(`Address_two`)}</p>
+            </div>
+          </div>
+          <div className="flex w-full flex-row px-8">
+            a
+          </div>
         </div>
       </section>
 
-      <section className="flex flex-col items-center justify-center mb-0">
+      <section className="mb-0 flex flex-col items-center justify-center">
         <h1 className="flex flex-col items-center gap-2">
           {t(`section_two_title`)}
           <hr className="w-12 border-t-2 border-black"></hr>
@@ -57,21 +94,21 @@ export default async function Index(props: IIndexProps) {
           </defs>
           <path fill="url(#gradient1)" fillOpacity="1" d="M0,20L60,24C120,28,240,36,360,46.7C480,57,600,69,720,74.7C840,80,960,80,1080,76C1200,72,1320,64,1380,59.3L1440,56L1440,180L1380,180C1320,180,1200,180,1080,180C960,180,840,180,720,180C600,180,480,180,360,180C240,180,120,180,60,180L0,180Z"></path>
         </svg>
-        <div className='w-full h-fit pt-8 pb-12 bg-gradient-to-r from-[#3DB2A1] to-[#1D265C] flex flex-row flex-wrap gap-12 justify-center items-center'>
-          <div className='flex flex-col text-white items-center w-96'>
-            <Events size="160" className='mb-8' />
-            <h2 className='mb-2'>{t(`section_three_one_title`)}</h2>
-            <h2 className='font-normal text-center'>{t(`section_three_one_description`)}</h2>
+        <div className="flex h-fit w-full flex-row flex-wrap items-center justify-center gap-12 bg-gradient-to-r from-[#3DB2A1] to-[#1D265C] pb-12 pt-8">
+          <div className="flex w-96 flex-col items-center text-white">
+            <Events size="160" className="mb-8" />
+            <h2 className="mb-2">{t(`section_three_one_title`)}</h2>
+            <h2 className="text-center font-normal">{t(`section_three_one_description`)}</h2>
           </div>
-          <div className='flex flex-col text-white items-center w-96'>
-            <Currency size="160" className='mb-8' />
-            <h2 className='mb-2'>{t(`section_three_two_title`)}</h2>
-            <h2 className='font-normal text-center'>{t(`section_three_two_description`)}</h2>
+          <div className="flex w-96 flex-col items-center text-white">
+            <Currency size="160" className="mb-8" />
+            <h2 className="mb-2">{t(`section_three_two_title`)}</h2>
+            <h2 className="text-center font-normal">{t(`section_three_two_description`)}</h2>
           </div>
-          <div className='flex flex-col text-white items-center w-96'>
-            <Shuttle size="160" className='mb-8' />
-            <h2 className='mb-2'>{t(`section_three_three_title`)}</h2>
-            <h2 className='font-normal text-center'>{t(`section_three_three_description`)}</h2>
+          <div className="flex w-96 flex-col items-center text-white">
+            <Shuttle size="160" className="mb-8" />
+            <h2 className="mb-2">{t(`section_three_three_title`)}</h2>
+            <h2 className="text-center font-normal">{t(`section_three_three_description`)}</h2>
           </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120">
@@ -85,7 +122,7 @@ export default async function Index(props: IIndexProps) {
         </svg>
       </section>
 
-      <section className='mx-20'>
+      <section className="mx-20">
         <h1>
           {t(`section_four_title`)}
           <hr className="w-10 border-t-2 border-black"></hr>
@@ -93,12 +130,12 @@ export default async function Index(props: IIndexProps) {
       </section>
 
       <section>
-        <div className='flex flex-row flex-wrap justify-center gap-20 w-full h-fit px-20 py-16 text-white bg-gradient-to-r from-[#1797C7] to-[#1D265C]'>
-          <div className='flex flex-col text-center justify-center w-2/5'>
-            <h2 className='mb-4'>{t(`section_five_title`)}</h2>
+        <div className="flex h-fit w-full flex-row flex-wrap justify-center gap-10 bg-gradient-to-r from-[#1797C7] to-[#1D265C] px-20 py-16 text-white lg:gap-20">
+          <div className="flex flex-col justify-center text-center lg:w-2/5">
+            <h2 className="mb-4">{t(`section_five_title`)}</h2>
             <p>{t(`section_five_description_one`)}</p>
             <p>{t(`section_five_description_two`)}</p>
-            <p className='font-bold'>{t(`section_five_description_three`)}</p>
+            <p className="font-bold">{t(`section_five_description_three`)}</p>
           </div>
           <Image className="w-[34.6875rem]" src={tcm_warehouse} alt="โกดัง" />
         </div>
@@ -111,6 +148,27 @@ export default async function Index(props: IIndexProps) {
           </defs>
           <path fill="url(#gradient3)" fillOpacity="1" d="M0,24L60,26.7C120,29,240,35,360,44C480,53,600,64,720,72C840,80,960,84,1080,81.3C1200,78,1320,69,1380,64L1440,60L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
         </svg>
+      </section>
+
+      <section className="mx-20">
+        <h1>
+          {t(`section_six_title`)}
+          <hr className="w-10 border-t-2 border-black"></hr>
+        </h1>
+        <div className="flex flex-wrap justify-center gap-8">
+          {articleItems.map(item => (
+            <div key={item} className="flex h-[32rem] w-56 flex-col border">
+              <div className="h-40 w-full bg-blue-500"></div>
+              <div className="grow p-3">
+                <h2>{t(`section_six_${item}_title_th` as `section_six_${ArticleKeys}_title_th`)}</h2>
+                <h2>{t(`section_six_${item}_title_en` as `section_six_${ArticleKeys}_title_en`)}</h2>
+                <p>
+                  {t(`section_six_${item}_description` as `section_six_${ArticleKeys}_description`)}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
