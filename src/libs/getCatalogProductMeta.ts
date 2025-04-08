db31f64f7ec7c408c02816f21e4722cd1f543f3b
wutpppppppppppppppppp <gansahy@gmail.com>
2025-04-08 20:43:48 +0700

    fix: delete console log and comments

--- src/libs/getCatalogProductMeta.ts
+++ src/libs/getCatalogProductMeta.ts
@@ -1,29 +1,4 @@
-// export async function getCatalogProductMeta(locale: string, slug: string) {
-//   const t = await getTranslations({ locale, namespace: 'Catalog' });
-
-//   // Get the whole catalog object
-//   const catalog = t.raw() as Record<string, any>;
-
-//   // Get the specific product
-//   const product = catalog[slug] as ProductMeta | undefined;
-
-//   if (!product) {
-//     return {
-//       title: catalog.meta_title,
-//       description: catalog.meta_description,
-//       keywords: catalog.keywords,
-//     };
-//   }
-
-//   return {
-//     title: product.meta_title || catalog.meta_title,
-//     description: product.meta_description || catalog.meta_description,
-//     keywords: product.keywords || catalog.keywords,
-//   };
-// }
-
 export async function getCatalogTranslation(locale: string) {
   const messages = await import(`@/locales/${locale}.json`);
-  console.error(messages.Catalog);
   return messages.Catalog;
 }
